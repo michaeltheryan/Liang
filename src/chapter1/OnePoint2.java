82ae61110e374324ad8758931bc02b4d1fb2ddf1
Michael Ryan <michaeltheryan@gmail.com>
2024-05-30 12:08:33 -0400

    completed exercise 2

---
+++ src/chapter1/OnePoint2.java
@@ -0,0 +1,12 @@
+package chapter1;
+
+public class OnePoint2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 1; i < 6; i++) {
+			System.out.println("Welcome to Java");
+		}
+	}
+
+}
