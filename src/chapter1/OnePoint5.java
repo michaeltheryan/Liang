0171d271107356ee1540b0df161b0e6c384e5213
Michael Ryan <michaeltheryan@gmail.com>
2024-08-12 15:41:42 -0400

    completed exercise 1.5

---
+++ src/chapter1/OnePoint5.java
@@ -0,0 +1,11 @@
+package chapter1;
+
+public class OnePoint5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// display 9.5 *4.5 - 2.5 *3 /	45.5 - 3.5
+		System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+	}
+
+}
